b5823208246ab32eba0ced2bbdde21f523db5bd5
Fábio André Damas <skkeeper@gmail.com>
2016-02-12 22:23:45 +0000

    wiki_to_json now works with cli parameters

--- converter/wiki_to_json.js
+++ converter/wiki_to_json.js
@@ -1,12 +1,23 @@
 #! /usr/bin/env node
+/**
+ * @file
+ * Converts lists taken from wikipedia pages like this:
+ * https://en.wikipedia.org/wiki/Wikipedia:Lists_of_common_misspellings/For_machines
+ * into a JSON format that can be read by common-typo-detector.
+ */
 
 'use strict';
 
 var readline = require('readline');
 var fs = require('fs');
 
+var parseArgs = require('minimist');
+var argv = parseArgs(process.argv.slice(2));
+
+var inputFile = argv._[0];
+
 var lineReader = readline.createInterface({
-  input: fs.createReadStream('common_misspellings_wikipedia.txt')
+  input: fs.createReadStream(inputFile)
 });
 
 var words = [];
