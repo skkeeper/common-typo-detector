9ef0b01f65be2f494d714a2cc534fb08369a173a
Fábio André Damas <skkeeper@gmail.com>
2016-02-12 22:30:33 +0000

    Add bunch of improvements
    Add return error exit code when typos are found.
    Files are now scanned sequently to avoid too many files opened at the same time.
    Add character column to the typo object.
    Shorten lines if they are too long when displaying the typo in context.
    typos and preset cli args can be used together.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var fs = require('fs');
 var path = require('path');
 var readline = require('readline');
+
 var chalk = require('chalk');
 var glob = require('glob');
 
@@ -14,37 +15,58 @@ var argv = parseArgs(process.argv.slice(2));
 var fileArgs = argv._;
 
 var installDirectory = path.dirname(require.main.filename);
-var typosJson = path.join(installDirectory, 'presets', `en.json`);
+var typosJson = [];
+
+if(typeof argv.typos === 'undefined' && typeof argv.preset === 'undefined') {
+  typosJson.push(path.join(installDirectory, 'presets', 'en.json'));
+}
 
 if (typeof argv.typos !== 'undefined') {
-  typosJson = argv.typos;
-} else if(typeof argv.preset !== 'undefined') {
-  typosJson = path.join(installDirectory, 'presets', `${argv.preset}.json`);
+  typosJson.push(argv.typos);
+}
+
+if(typeof argv.preset !== 'undefined') {
+  typosJson.push(path.join(installDirectory, 'presets', `${argv.preset}.json`));
 }
 
 function loadTypos () {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(typosJson, 'utf8', function (err, contents) {
-      resolve(JSON.parse(contents));
+  let promises = [];
+  for(let i = 0; i < typosJson.length; i++) {
+    promises.push(new Promise(function (resolve, reject) {
+      fs.readFile(typosJson[i], 'utf8', function (err, contents) {
+        resolve(JSON.parse(contents));
+      });
+    }));
+  }
+
+  return Promise.all(promises).then(function (jsons) {
+    return new Promise(function (resolve, reject) {
+      let concatenated = [];
+      for(let j = 0; j < jsons.length; j++) {
+        concatenated = concatenated.concat(jsons[j]);
+      }
+
+      resolve(concatenated);
     });
   });
 }
 
 function scanFileForTypos (file, typos) {
   return new Promise(function (resolve, reject) {
-    var lineReader = readline.createInterface({
+    let lineReader = readline.createInterface({
       input: fs.createReadStream(file)
     });
     let lineNumber = 1;
     let typosFound = [];
     lineReader.on('line', function (line) {
-      for (var i = 0; i < typos.length; i++) {
-        var pattern = `\\b${typos[i].typo}\\b`;
-
-        if (new RegExp(pattern, 'g').test(line)) {
+      for (let i = 0; i < typos.length; i++) {
+        let pattern = `\\b${typos[i].typo}\\b`;
+        let matchIndex = line.search(pattern);
+        if (matchIndex > -1) {
           typosFound.push({
             file: file,
             lineNumber: lineNumber,
+            charColumn: matchIndex + 1,
             typo: typos[i].typo,
             lineText: line,
             correction: typos[i].correction
@@ -89,31 +111,43 @@ function highlightTypo (text, typo) {
   let preTypo = text.substring(0, typoIndex);
   let postTypo = text.substring(typoIndex + typo.length);
 
+  if(text.length > 60) {
+    // TODO: Improve what to include around the typo
+    preTypo = preTypo.substring(preTypo.length - 40);
+    postTypo = postTypo.substring(0, 10);
+  }
+
   return preTypo + chalk.bold.red(typo) + postTypo;
 }
 
 loadTypos().then(function (typos) {
   getFilesFromGlobPatterns(fileArgs).then(function (filesMatching) {
-    let promises = [];
 
-    for (let i = 0; i < filesMatching.length; i++) {
-      promises.push(scanFileForTypos(filesMatching[i], typos));
-    }
+    let concatenatedResults = [];
+    var promise = Promise.resolve(null);
 
-    Promise.all(promises).then(function (result) {
-      let concatenatedResults = [];
-      for (let i = 0; i < result.length; i++) {
-        concatenatedResults = concatenatedResults.concat(result[i]);
-      }
+    filesMatching.forEach(function (value) {
+      promise = promise.then(function () {
+        return scanFileForTypos(value, typos);
+      }).then(function (newValue) {
+        concatenatedResults = concatenatedResults.concat(newValue);
+      });
+    });
 
-      for (var i = 0; i < concatenatedResults.length; i++) {
+    return promise.then(function (result) {
+      for (let i = 0; i < concatenatedResults.length; i++) {
         let result = concatenatedResults[i];
+        console.log();
         console.log(chalk.bold.yellow('file: ') + result.file);
         console.log(chalk.bold.yellow('line number: ') + result.lineNumber);
-        console.log(chalk.bold.yellow('suggestion: ') + result.correction);
+        console.log(chalk.bold.yellow('character column: ') + result.charColumn);
         console.log(chalk.bold.yellow('line: '));
         console.log(highlightTypo(result.lineText, result.typo));
-        console.log();
+        console.log(chalk.bold.yellow('suggestion: ') + result.correction);
+      }
+
+      if(concatenatedResults.length > 0) {
+        process.exit(1);
       }
     });
   });
